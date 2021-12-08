8088a7220a9ad05454674daa71d99ac72e886138
Lana <D0745969@o365.fcu.edu.tw>
2021-12-08 20:51:44 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,11 @@
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #2c3e50;
-  padding: 0
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss" scoped>
+#nav{
+  background-color: #000
 }
 </style>
+
+
